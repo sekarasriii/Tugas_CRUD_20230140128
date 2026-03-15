85c8982f0be597153d5a1a01202c1f51569a6bdb
sekarasriii <sekareeesh@gmail.com>
2026-03-15 21:14:01 +0700

    menambahkan setup untuk koneksi database dan membuat entitas ktp

---
+++ src/main/java/com/tugas/Tugas_CRUD_20230140128/entity/Ktp.java
@@ -0,0 +1,31 @@
+package com.tugas.Tugas_CRUD_20230140128.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "ktp")
+public class Ktp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, nullable = false)
+    private String nomorKtp;
+
+    @Column(nullable = false)
+    private String namaLengkap;
+
+    private String alamat;
+
+    private LocalDate tanggalLahir;
+
+    private String jenisKelamin;
+}
