4e9df7f9ddaf95749d9acd7585be70a887ccc7f2
sekarasriii <sekareeesh@gmail.com>
2026-03-15 22:59:07 +0700

    membuat dokumentasi  API dan screenshot tampilan website ke dalam Readme

---
+++ src/main/java/com/tugas/Tugas_CRUD_20230140128/TugasCrud20230140128Application.java
@@ -0,0 +1,13 @@
+package com.tugas.Tugas_CRUD_20230140128;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TugasCrud20230140128Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TugasCrud20230140128Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tugas/Tugas_CRUD_20230140128/TugasCrud20230140128ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tugas.Tugas_CRUD_20230140128;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TugasCrud20230140128ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
