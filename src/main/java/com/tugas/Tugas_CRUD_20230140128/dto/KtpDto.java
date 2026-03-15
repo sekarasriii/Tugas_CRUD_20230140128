ee941e9f898222e26afd4474bfd18070326c127d
sekarasriii <sekareeesh@gmail.com>
2026-03-15 21:18:44 +0700

    menambahkan setup dto sebagainformat data frontend

---
+++ src/main/java/com/tugas/Tugas_CRUD_20230140128/dto/KtpDto.java
@@ -0,0 +1,14 @@
+package com.tugas.Tugas_CRUD_20230140128.dto;
+
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
+public class KtpDto {
+    private Integer id;
+    private String nomorKtp;
+    private String namaLengkap;
+    private String alamat;
+    private LocalDate tanggalLahir;
+    private String jenisKelamin;
+}
