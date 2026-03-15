e8dfa16b513c8a070a860f5ee7b46972b6422190
sekarasriii <sekareeesh@gmail.com>
2026-03-15 21:22:10 +0700

    menambahkan KtpMapper menggunakan MapStruct/tambahan component model spring

---
+++ src/main/java/com/tugas/Tugas_CRUD_20230140128/mapper/KtpMapper.java
@@ -0,0 +1,19 @@
+package com.tugas.Tugas_CRUD_20230140128.mapper;
+
+import com.tugas.Tugas_CRUD_20230140128.dto.KtpDto;
+import com.tugas.Tugas_CRUD_20230140128.entity.Ktp;
+import org.mapstruct.Mapper;
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface KtpMapper {
+
+    // Mengubah Entity menjadi DTO
+    KtpDto toDto(Ktp ktp);
+
+    // Mengubah DTO menjadi Entity
+    Ktp toEntity(KtpDto ktpDto);
+
+    // Mengubah List Entity menjadi List DTO (berguna saat get all KTP)
+    List<KtpDto> toDtoList(List<Ktp> ktpList);
+}
