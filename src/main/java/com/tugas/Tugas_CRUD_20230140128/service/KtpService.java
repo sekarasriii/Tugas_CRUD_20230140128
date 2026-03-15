f08cc4a007fc5ba81e9bb3b0b6ef021c8876328d
sekarasriii <sekareeesh@gmail.com>
2026-03-15 21:40:18 +0700

    membuat interface untuk ktp service

---
+++ src/main/java/com/tugas/Tugas_CRUD_20230140128/service/KtpService.java
@@ -0,0 +1,12 @@
+package com.tugas.Tugas_CRUD_20230140128.service;
+
+import com.tugas.Tugas_CRUD_20230140128.dto.KtpDto;
+import java.util.List;
+
+public interface KtpService {
+    KtpDto createKtp(KtpDto ktpDto);
+    List<KtpDto> getAllKtp();
+    KtpDto getKtpById(Integer id);
+    KtpDto updateKtp(Integer id, KtpDto ktpDto);
+    void deleteKtp(Integer id);
+}
